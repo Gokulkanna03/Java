857482a5ac62c5feebdc036ec1112cc1408b0018
gokulkanna0103 <gokulgokulkannas@gmail.com>
2025-12-13 12:47:46 +0530

    init

---
+++ Cp.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+public class Cp
+
+{
+	public static void main(String[] args) {
+	    Scanner obj=new Scanner(System.in);
+	    	System.out.print("enter selling :");
+	    double selling=obj.nextDouble();
+		System.out.print("Enter Loss in % :");
+		double loss=obj.nextDouble();
+		double los=(1-loss/100);
+		System.out.printf("Cost price :"+"%.2f",selling/los);
+		obj.close();
+	}
+}
